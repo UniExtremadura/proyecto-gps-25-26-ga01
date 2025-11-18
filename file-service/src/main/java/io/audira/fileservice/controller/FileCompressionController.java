eb723cf3377de15ab369f98c9753cd0f99653637
ManahenGarciaGarrido <manahengag@gmail.com>
2025-11-18 23:11:46 +0100

    Vuelta a IP correcta

--- file-service/src/main/java/io/audira/fileservice/controller/FileCompressionController.java
+++ file-service/src/main/java/io/audira/fileservice/controller/FileCompressionController.java
@@ -17,7 +17,7 @@ public class FileCompressionController {
 
     private final FileCompressionService fileCompressionService;
 
-    @Value("${file.base-url:http://68.221.240.178:9005}")
+    @Value("${file.base-url:http://188.227.140.98:9005}")
     private String baseUrl;
 
     @PostMapping("/compress")
